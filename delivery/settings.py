076bb7b7b4185f7dbc617950e3bb725ade2fb010
Jijiluzana225 <jiji.luzana@gmail.com>
2025-01-14 06:30:59 +0800

    Update settings.py
    j

--- delivery/settings.py
+++ delivery/settings.py
@@ -39,9 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles', 
-    'cloudinary_storage',
     'cloudinary',
-    
+    'cloudinary_storage',      
     'store',
 ]
 
