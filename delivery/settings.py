639e9c9f12b410dc716b3379fc7689ea2746666d
Jijiluzana225 <jiji.luzana@gmail.com>
2025-01-13 19:55:47 +0800

    Update settings.py
    sff

--- delivery/settings.py
+++ delivery/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h+fyj)z-4rqjgi+ml2l21xh@9s_^=!u12us$u-e$gthp7_8qyn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
