34883d4ef1d804867e127afdd6d09c9567cfb6cc
Jijiluzana225 <jiji.luzana@gmail.com>
2025-01-13 18:44:08 +0800

    Update settings.py
    adfad

--- delivery/settings.py
+++ delivery/settings.py
@@ -31,6 +31,7 @@ CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
