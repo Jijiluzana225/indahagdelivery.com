bc98d06fba12630566a08b85e197a8f743578015
Jijiluzana225 <jiji.luzana@gmail.com>
2025-01-14 11:56:00 +0800

    Create envy.py
    ADFAS

---
+++ envy.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["SECRET_KEY"] = 'abNwZ-xrMC9_GSberAXBAIufCA4'
+os.environ["DEVELOPMENT"] = "Tue"
+os.environ["CLOUDINARY_URL"] = "cloudinary://637252742851763:abNwZ-xrMC9_GSberAXBAIufCA4@dyiqfwpib"
