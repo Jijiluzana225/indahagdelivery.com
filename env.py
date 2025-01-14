8f856a17f1f522a76065119917cbcd1bcb51ea16
Jijiluzana225 <jiji.luzana@gmail.com>
2025-01-14 12:05:47 +0800

    Update env.py
    adsfadf

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import os
 
 os.environ["SECRET_KEY"] = 'django-insecure-h+fyj)z-4rqjgi+ml2l21xh@9s_^=!u12us$u-e$gthp7_8qyn'
-os.environ["DEVELOPMENT"] = "Tue"
+os.environ["DEVELOPMENT"] = "True"
 os.environ["CLOUDINARY_URL"] = "cloudinary://637252742851763:abNwZ-xrMC9_GSberAXBAIufCA4@dyiqfwpib"
