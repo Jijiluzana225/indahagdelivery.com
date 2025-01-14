1ca77dc742d0adf955a3526e7596ed6e428ee766
Jijiluzana225 <jiji.luzana@gmail.com>
2025-01-14 08:29:58 +0800

    Update settings.py
    adad

--- delivery/settings.py
+++ delivery/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import cloudinary_storage
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,16 +33,15 @@ ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
 # Application definition
 
-INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
+INSTALLED_APPS = [    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles', 
-    'cloudinary',
+    'django.contrib.staticfiles',     
     'cloudinary_storage',      
+    'cloudinary',
     'store',
 ]
 
@@ -134,7 +135,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
@@ -145,7 +146,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = BASE_DIR / 'media'
-
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': ' dyiqfwpib',
     'API_KEY': '637252742851763',
