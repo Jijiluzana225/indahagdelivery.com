9bb30e2db1837e5cf1e5082cc1b106d72c0b98d7
Jijiluzana225 <jiji.luzana@gmail.com>
2025-01-15 16:05:44 +0800

    Update models.py
    new

--- store/models.py
+++ store/models.py
@@ -79,7 +79,11 @@ class Order(models.Model):
         ],
         default='Pending'
     )
+    
+    money = models.DecimalField(max_digits=10, decimal_places=2,blank=True, null=True)
+    instructions=models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    
     
     def __str__(self):
         return f"Order {self.id} - {self.customer.username} - {self.status}"
