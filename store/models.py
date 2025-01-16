b16616e0ab9f2e88c8f59ca68ea98a9d9d9b2ab1
Jijiluzana225 <jiji.luzana@gmail.com>
2025-01-16 16:05:44 +0800

    Update models.py
    ads

--- store/models.py
+++ store/models.py
@@ -83,6 +83,7 @@ class Order(models.Model):
     money = models.DecimalField(max_digits=10, decimal_places=2,blank=True, null=True)
     instructions=models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, db_index=True)
     
     
     def __str__(self):
