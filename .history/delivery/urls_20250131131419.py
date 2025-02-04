8c0605abfd9c3edd77fe088f71e581c55732167a
Jijiluzana225 <jiji.luzana@gmail.com>
2025-02-04 09:28:44 +0800

    sounds

--- .history/delivery/urls_20250131131419.py
+++ .history/delivery/urls_20250131131419.py
@@ -7,7 +7,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),  # assuming you will create store URLs
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    
