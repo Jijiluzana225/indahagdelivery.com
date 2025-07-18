8e6b73cf2a978b4dc694f4896af1da94a145f469
Jijiluzana225 <jiji.luzana@gmail.com>
2025-07-18 20:03:16 +0800

    adf

--- store/urls.py
+++ store/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # Add URLs for different views
     path('', views.system_update, name='system_update'),  # List of all stores
     # path('', views.system_update_close, name='system_update_close'),  # List of all stores
+    
     path('x1', views.store_list, name='store_list'),  # List of all stores 
     path('product/<int:id>/', views.product_detail, name='product_detail'),  # Product detail page
     # path('store/<int:id>/', views.store_detail, name='store_detail'),  # Store detail page
