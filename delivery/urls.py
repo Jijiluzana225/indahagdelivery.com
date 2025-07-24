5caef0084e82e0591baf73a13b51bb7a5b85db46
Jijiluzana225 <jiji.luzana@gmail.com>
2025-07-24 14:51:21 +0800

    assdf

--- delivery/urls.py
+++ delivery/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),  # assuming you will create store URLs
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
